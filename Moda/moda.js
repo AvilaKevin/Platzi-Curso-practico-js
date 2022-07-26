c53508355a7e2e69bd9ba0f7d9ce40a7c20dd329
ViperDex <viperdex2000@hotmailc.om>
2022-07-26 15:46:03 -0500

    Logica ponderado

--- Moda/moda.js
+++ Moda/moda.js
@@ -1,40 +1,36 @@
-var lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    3,
-];
+/// --- -- CONEXION HTML -- --- ///
+function resultadoModa() {
+    // VARIABLES
+    const lista = document.getElementById("input-lista").value;
+    const ResultadoP = document.getElementById("ResultadoP");
+    const listaModificada = lista.split(",");
+    const numberArray = listaModificada.map(Number);
 
+    // CONTADOR
+    listaCount = {};
 
-lista1Count = {};
-
-
-lista1.map(
-    function (elemento){
-        if (lista1Count[elemento]){
-            lista1Count[elemento] = lista1Count[elemento] + 1;
-            // lista1Count[elemento] += 1; // Es es lo mismo q arriba pero mas corto.
-        }else{
-            lista1Count[elemento] = 1;
+    numberArray.map(
+        function (elemento) {
+            if (listaCount[elemento]) {
+                listaCount[elemento] = listaCount[elemento] + 1;
+            } else {
+                listaCount[elemento] = 1;
+            }
         }
-    }
-);
+    );
 
+    // CONVERSOR
+    const ArrayConvert = Object.entries(listaCount).sort(
+        function (valorAcumulado, nuevoValor) {
+            return valorAcumulado[1] - nuevoValor[1];
+        }
+    );
 
-const ArrayConvert = Object.entries(lista1Count).sort( // Object.entries nos transforma un elemento en un array.
-    function (valorAcumulado, nuevoValor){
-        return valorAcumulado[1] - nuevoValor[1]; // nuevoValor[1] se hace de este modo para que se realice la validacion sobre la segunda posicion, la cual nos indica cual se repite mas.
-    }
-);
+    const moda = ArrayConvert[ArrayConvert.length - 1];
 
-// la  moda es la ultima posicion de ArrayConvert asi que se debe hacer lo siguiente:
+    var valorIngresado = moda[0];
+    var repeticiones = moda[1];
 
-const moda = ArrayConvert[ArrayConvert.length - 1]; // array.length = 4 elementos por lo tanto para acceder al ultimo elemento debo resta 1 puesto que el array inicia en 0.
-
-var valorIngresado = moda[0];
-var repeticiones = moda[1];
-
-console.log("El valor: " + valorIngresado + " Se repite: " + repeticiones + " veces");
+    // RESULTADO
+    ResultadoP.innerText = ("El numero: " + valorIngresado + " se repite: " + repeticiones + " veces");
+}
--- Ponderado/ponderado.js
+++ Ponderado/ponderado.js
@@ -14,12 +14,12 @@ var ponderado = [
 ]
 
 
-//1. Se crea un array el cueal almacena el resultado del for
+//1. Se crea un array el cual almacena el resultado del for
 var Arraysuma = [];
 
 
 //2. Leer el array, traera la primera posicion del array, realizar la operacion, almacenar los resultados en un nuevo array, y asi con todos los objetos del array.
-for ( var i = 0; i < ponderado.length; i++ ){
+for (var i = 0; i < ponderado.length; i++) {
     var objeto = ponderado[i]; // Esta variable almacena el objeto del array, para luego acceder a las propiedades del mismo.
     var multiplicacion = objeto.nota * objeto.creditos; // se accede a las propiedades del objeto.
     Arraysuma.push(multiplicacion); // aqui se indica en q array se gurda el resultado.
@@ -33,24 +33,64 @@ for ( var i = 0; i < ponderado.length; i++ ){
 
 
 //3. Se lee el Arraysuma y se realiza la suma de los valores.
-var sumaMultiplicacion = Arraysuma.reduce( 
-    function (valorAcumulado, nuevoValor){
+var sumaMultiplicacion = Arraysuma.reduce(
+    function (valorAcumulado, nuevoValor) {
         return valorAcumulado + nuevoValor;
     });
 
 
 //4. Se extrae los creditos en un array por separado.
-var creditos = ponderado.map( ArrayCreditos => ArrayCreditos.creditos);
+var creditos = ponderado.map(ArrayCreditos => ArrayCreditos.creditos);
 
 
 //5. Se lee el array de los creditos y se realiza la suma de estos.
 // El metodo reduce ejecuta una funcion por cada elemento del array.
-var sumaCreditos = creditos.reduce( 
-    function (creditosAcumulados, nuevosCreditos){
+var sumaCreditos = creditos.reduce(
+    function (creditosAcumulados, nuevosCreditos) {
         return creditosAcumulados + nuevosCreditos;
     });
 
 //6. Se crea variable para dar resultado
-const resultado = sumaMultiplicacion/sumaCreditos;
+const resultado = sumaMultiplicacion / sumaCreditos;
 
-console.log(resultado);
+//Se crea contador para poder identificar los nuevos inputs que se van creando
+contadorNota = 1;
+
+// CONEXION HTML
+function agregaCasillas() {
+    //Se crea contador
+    contadorNota++;
+
+    //Se crea el elemento / en este caso es el contenedor
+    var div = document.createElement('div');
+    //Se le agregan atributos al alemento
+    div.setAttribute('class', 'nota_' + contadorNota);
+
+    //Se procede introducir elementos dentro del div
+    div.innerHTML = '<label class="form__labels" for="input-notas"> Nota ' + contadorNota + '<input class="form__input form__input--margin_left" id="input_notas_' + contadorNota + '" type="number" placeholder="Notas" /></label><label class="form__labels" for="input-creditos"><input class="form__input form__input--margin_left" id="input_creditos_' + contadorNota + '" type="number" placeholder="Creditos" /></label>';
+
+    //Se renderiza el div /
+    document.getElementById('valores').appendChild(div);
+
+    // si queremos utilizar el contador para otras cosas se usa el return
+    return contadorNota;
+}
+
+function resultadoPonderado() {
+    //Se crea array para almacenar los objetos
+    let valores = [];
+
+    //Mediante un for se procede a capturar los datos del input utilizando como identificador el contador
+    for (var i = 0; i < contadorNota; i++) {
+        const inputNota = parseInt(document.getElementById('input_notas_' + contadorNota).value);
+        const inputCreditos = parseInt(document.getElementById('input_creditos_' + contadorNota).value);
+        //Por cada nota se crea un objeto
+        const objetoNotas = {
+            nota: inputNota,
+            creditos: inputCreditos
+        };
+        //Se almacena el objeto dentro del array
+        valores.push(objetoNotas);
+        console.log(valores);
+    }
+}
