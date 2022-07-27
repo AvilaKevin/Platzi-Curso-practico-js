c6178c4af37ae5e073d13261966c0460ca54e419
ViperDex <viperdex2000@hotmailc.om>
2022-07-27 17:46:27 -0500

    ponderado almost completed

--- Ponderado/ponderado.js
+++ Ponderado/ponderado.js
@@ -13,46 +13,10 @@ var ponderado = [
     }
 ]
 
-
-//1. Se crea un array el cual almacena el resultado del for
-var Arraysuma = [];
-
-
-//2. Leer el array, traera la primera posicion del array, realizar la operacion, almacenar los resultados en un nuevo array, y asi con todos los objetos del array.
-for (var i = 0; i < ponderado.length; i++) {
-    var objeto = ponderado[i]; // Esta variable almacena el objeto del array, para luego acceder a las propiedades del mismo.
-    var multiplicacion = objeto.nota * objeto.creditos; // se accede a las propiedades del objeto.
-    Arraysuma.push(multiplicacion); // aqui se indica en q array se gurda el resultado.
-}
-
-
 // // El paso 2 se pudo hacer con el metodo .map:
 // const notesWithCredit = ponderado.map(function (noteObject) {
 //     return noteObject.nota * noteObject.creditos;
 // });
-
-
-
-//3. Se lee el Arraysuma y se realiza la suma de los valores.
-var sumaMultiplicacion = Arraysuma.reduce(
-    function (valorAcumulado, nuevoValor) {
-        return valorAcumulado + nuevoValor;
-    });
-
-
-//4. Se extrae los creditos en un array por separado.
-var creditos = ponderado.map(ArrayCreditos => ArrayCreditos.creditos);
-
-
-//5. Se lee el array de los creditos y se realiza la suma de estos.
-// El metodo reduce ejecuta una funcion por cada elemento del array.
-var sumaCreditos = creditos.reduce(
-    function (creditosAcumulados, nuevosCreditos) {
-        return creditosAcumulados + nuevosCreditos;
-    });
-
-//6. Se crea variable para dar resultado
-const resultado = sumaMultiplicacion / sumaCreditos;
 
 const contenedor = document.getElementById('valores');
 
@@ -77,11 +41,10 @@ function agregaCasillas() {
     return contadorNota;
 }
 
-//Se crea array para almacenar los objetos
-let arrayValores = [];
 
-function resultadoPonderado() {
-    let arrayValores = [];
+function almacenamientoNotas() {
+    // //Se recetea el array
+    let arrayNotas = [];
 
     //Mediante un for se procede a capturar los datos del input utilizando como identificador el contador
     for (var i = 1; i <= contadorNota; i++) {
@@ -95,13 +58,52 @@ function resultadoPonderado() {
         };
 
         //Se almacena el objeto dentro del array
-        arrayValores.push(objetoNotas);
+        arrayNotas.push(objetoNotas);
     }
 
-    // arrayValores.indexOf(input[, fromIndex])
+    //Se devuelve array para usarlo posteriormente
+    return arrayNotas;
+}
 
-    console.log(arrayValores);
-    return arrayValores;
+
+function resultadoPonderado() {
+    const resultadoHtml = document.getElementById('ResultadoP');
+    let arrayNotas = almacenamientoNotas();
+    //1. Se crea un array el cual almacena el resultado del for
+    var Arraysuma = [];
+
+
+    //2. Leer el array, traera la primera posicion del array, realizar la operacion, almacenar los resultados en un nuevo array, y asi con todos los objetos del array.
+    for (var i = 0; i < arrayNotas.length; i++) {
+        var objeto = arrayNotas[i]; // Esta variable almacena el objeto del array, para luego acceder a las propiedades del mismo.
+        var multiplicacion = objeto.nota * objeto.creditos; // se accede a las propiedades del objeto.
+        Arraysuma.push(multiplicacion); // aqui se indica en q array se gurda el resultado.
+    }
+
+
+    //3. Se lee el Arraysuma y se realiza la suma de los valores.
+    var sumaMultiplicacion = Arraysuma.reduce(
+        function (valorAcumulado, nuevoValor) {
+            return valorAcumulado + nuevoValor;
+        });
+
+
+    //4. Se extrae los creditos en un array por separado.
+    var creditos = arrayNotas.map(ArrayCreditos => ArrayCreditos.creditos);
+
+
+    //5. Se lee el array de los creditos y se realiza la suma de estos.
+    // El metodo reduce ejecuta una funcion por cada elemento del array.
+    var sumaCreditos = creditos.reduce(
+        function (creditosAcumulados, nuevosCreditos) {
+            return creditosAcumulados + nuevosCreditos;
+        });
+
+    //6. Se crea variable para dar resultado
+    const resultado = sumaMultiplicacion / sumaCreditos;
+
+    console.log(resultado);
+    resultadoHtml.innerHTML = resultado;
 }
 
 
